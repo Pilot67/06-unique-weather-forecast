31665758131d5b6b5ae41a933620d85b47c0e66d
Stuart Simmons <stuart@Stuart.local>
2021-09-27 09:36:53 +1000

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,14 @@ fetch(weatherUrl+appId)
     return response.json();
   })
   .then(function (data) {
-    //console.log(data);
     if (data.cod === '404' && data.message === 'city not found' ){
       throw new Error('404 city not found');
     }else if (data.cod === 200){
-      if (addToList){addSearch()}; // add city to the search list
+      // add city to the search list
+      if (addToList){
+        addSearch($('#ask').val().trim())
+        saveList();
+      }; 
 
       // do something with data
       weatherData = data;
@@ -29,7 +32,7 @@ fetch(weatherUrl+appId)
   })
   .then(function(data){
       //setup and call OneCall
-      var oneCallUrl = 'http://api.openweathermap.org/data/2.5/onecall?lat='+data.coord.lat+'&lon='+data.coord.lon;
+      var oneCallUrl = 'http://api.openweathermap.org/data/2.5/onecall?lat='+data.coord.lat+'&lon='+data.coord.lon+'&units=metric';
       getOneCallData(oneCallUrl);
   })
 
@@ -43,37 +46,88 @@ function getOneCallData(oneCallUrl){
   })
   .then(function (data) {
     forecastData = data
-    console.log(weatherData)
-    console.log(forecastData)
     printWeather();
   });
 }
 
 function printWeather(){
-  //this function print the current and forecast weather to the screen
-  var todayTitleEl = $('<h5>').text('Todays weather for ' + weatherData.name);
-  $('#todaysWeather').append(todayTitleEl);
+  //this function prints the current and forecast weather to the screen
+  $('#todaysWeather').text(""); //clear the children
+  console.log(weatherData);
+  console.log(forecastData);
 
+  var bgArr = ['green', 'yellow', 'darkorange','red', 'purple']
+
+  var weatherDate = moment.unix(weatherData.dt).utc().utcOffset(weatherData.timezone/60).format('D-M-YYYY');
+  var todayTitleEl = $('<h3>').attr({class:'p-2'}).text('Current weather for ' + weatherData.name + ' ' + weatherDate);
+  var tempEl = $('<h5>').attr({class:'p-2'}).text('Temperature ' + weatherData.main.temp + "\xB0C");
+  var windEl = $('<h5>').attr({class:'p-2'}).text('Wind speed ' + weatherData.wind.speed + " km/h")
+  var humidityEl = $('<h5>').attr({class:'p-2'}).text('Humidity ' + weatherData.main.humidity + "%")
+  var uviEl = $('<p>').attr({class:'p-2'})
+  var uviElSpan = $('<span>').attr({class:'px-2 rounded'}).css('background-color',bgArr[Math.round((forecastData.current.uvi-1)/2.8)],).text('UV Index ' + forecastData.current.uvi)
+  uviEl.append(uviElSpan);
+
+  $('#todaysWeather').append(todayTitleEl);
+  $('#todaysWeather').append(tempEl);
+  $('#todaysWeather').append(windEl);
+  $('#todaysWeather').append(humidityEl);
+  $('#todaysWeather').append(uviEl);
+
+  // this section for the daily forecast
+  //<div id='forcast0' class = 'col-12 col-sm border'>
+  //<h6>forecast day 0</h6>
+  //</div>
+
+  $('#forecastWrapper').text('') // clear the wrapper
+  for (var i = 1; i < 6; i++){
+    weatherDate = moment.unix(forecastData.daily[i].dt).utc().utcOffset(weatherData.timezone/60).format('D-M-YYYY');
+    var day = $('<div>').attr({class:'col-12 col-sm border mx-1'});
+    var dateEl = $('<h5>').attr({class:'text-center'}).text(weatherDate);
+
+    day.append(dateEl);
+    $('#forecastWrapper').append(day);
+  }
 }
 
 
-function addSearch(){
+
+function addSearch(itemText){
   var numChildren = $('#sortable li').length
   console.log(numChildren)
-  newLineEl = $('<li>').attr({class:'d-flex justify-content-between ui-state-default'}).text($('#ask').val().trim());
+//  newLineEl = $('<li>').attr({class:'d-flex justify-content-between ui-state-default'}).text($('#ask').val().trim());
+  newLineEl = $('<li>').attr({class:'d-flex justify-content-between ui-state-default'}).text(itemText);
   newLineEl.append($('<span>').attr({class:'ui-icon ui-icon-arrowthick-2-n-s'}));
   newLineEl.append($('<i>').attr({class:'btn btn-danger p-1 fas fa-trash-alt',type:'button'}));
   $('#sortable').append(newLineEl);
   $('#ask').val("");
-  //add code here to save the list to local storage
 }
+
+function saveList(){
+  var listArr = [];
+    for (var i = 0; i < $('#sortable li').length; i++){
+      listArr[i] = $('#sortable li:nth-child('+ (i+1) +')').text().trim();
+    }
+  localStorage.setItem('CityList', JSON.stringify(listArr));
+}
+
+function loadList(){
+  $('#sortable').text(''); //clear the sortable list
+  var listArr = JSON.parse(localStorage.getItem('CityList'))
+  if(listArr) {
+    for (var i = 0; i < listArr.length; i++){
+      addSearch(listArr[i]);
+    }
+}
+}
+
+loadList();
 
 
 // remove item from the sortable list
 $('#sortable').on('click', 'i', function(event){
     console.log("Trash Clicked")
     $(event.target).parent().remove()
-    //add code here to re-save the list
+    saveList();
 })
 
 $('#sortable').on('click', 'li', function(event){
@@ -105,6 +159,7 @@ $( function() {
 });
 // function for jquery-ui sortable list
 $( function() {
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
+  $( "#sortable" ).sortable();
+  $( "#sortable" ).sortable({cursor: 'move'});
+  $( "#sortable" ).disableSelection();
 });
