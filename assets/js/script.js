6e452f728a91df73b49212a1e7ba280909e88e81
Stuart Simmons <stuart@Stuart.local>
2021-09-27 13:03:42 +1000

    Added colours and icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,41 +60,53 @@ function printWeather(){
 
   var weatherDate = moment.unix(weatherData.dt).utc().utcOffset(weatherData.timezone/60).format('D-M-YYYY');
   var todayTitleEl = $('<h3>').attr({class:'p-2'}).text('Current weather for ' + weatherData.name + ' ' + weatherDate);
+  var iconElSpan = $('<span>');
+  var iconEl = $('<img>').attr({src:'http://openweathermap.org/img/wn/'+ weatherData.weather[0].icon + '@2x.png'});
+  var descEl = $('<h5>').attr({class:'p-2'}).text(weatherData.weather[0].description);
   var tempEl = $('<h5>').attr({class:'p-2'}).text('Temperature ' + weatherData.main.temp + "\xB0C");
   var windEl = $('<h5>').attr({class:'p-2'}).text('Wind speed ' + weatherData.wind.speed + " km/h")
   var humidityEl = $('<h5>').attr({class:'p-2'}).text('Humidity ' + weatherData.main.humidity + "%")
-  var uviEl = $('<p>').attr({class:'p-2'})
+  var uviEl = $('<h5>').attr({class:'p-2'})
   var uviElSpan = $('<span>').attr({class:'px-2 rounded'}).css('background-color',bgArr[Math.round((forecastData.current.uvi-1)/2.8)],).text('UV Index ' + forecastData.current.uvi)
   uviEl.append(uviElSpan);
-
+  iconElSpan.append(iconEl);
+  todayTitleEl.append(iconElSpan);
+  
   $('#todaysWeather').append(todayTitleEl);
+  $('#todaysWeather').append(descEl);
   $('#todaysWeather').append(tempEl);
   $('#todaysWeather').append(windEl);
   $('#todaysWeather').append(humidityEl);
   $('#todaysWeather').append(uviEl);
 
-  // this section for the daily forecast
-  //<div id='forcast0' class = 'col-12 col-sm border'>
-  //<h6>forecast day 0</h6>
-  //</div>
-
+  // 5 day forecast
   $('#forecastWrapper').text('') // clear the wrapper
   for (var i = 1; i < 6; i++){
     weatherDate = moment.unix(forecastData.daily[i].dt).utc().utcOffset(weatherData.timezone/60).format('D-M-YYYY');
-    var day = $('<div>').attr({class:'col-12 col-sm border mx-1'});
+    var day = $('<div>').attr({class:'col-12 col-md border m-1 p-1'});
     var dateEl = $('<h5>').attr({class:'text-center'}).text(weatherDate);
+    var iconElUrl = 'http://openweathermap.org/img/wn/' + forecastData.daily[i].weather[0].icon + '.png';
+    iconEl = $('<img>').attr({src:iconElUrl,id:'forecastIcon', class:'justify-content-center'});
+    descEl = $('<p>').text(forecastData.daily[i].weather[0].description);
+    var tempMinEl =$('<p>').text('Min: ' + Math.round(forecastData.daily[i].temp.min)+ "\xB0C");
+    var tempMaxEl =$('<p>').text('Max: ' + Math.round(forecastData.daily[i].temp.max)+ "\xB0C");
+    windEl =$('<p>').text('Wind: ' + forecastData.daily[i].wind_speed + 'km/h');
+    humidityEl = $('<p>').text('Humidity: ' + forecastData.daily[i].humidity+'%')
 
     day.append(dateEl);
+    day.append(iconEl);
+    day.append(descEl);
+    day.append(tempMinEl);
+    day.append(tempMaxEl);
+    day.append(windEl);
+    day.append(humidityEl);
+
     $('#forecastWrapper').append(day);
+
   }
 }
 
-
-
 function addSearch(itemText){
-  var numChildren = $('#sortable li').length
-  console.log(numChildren)
-//  newLineEl = $('<li>').attr({class:'d-flex justify-content-between ui-state-default'}).text($('#ask').val().trim());
   newLineEl = $('<li>').attr({class:'d-flex justify-content-between ui-state-default'}).text(itemText);
   newLineEl.append($('<span>').attr({class:'ui-icon ui-icon-arrowthick-2-n-s'}));
   newLineEl.append($('<i>').attr({class:'btn btn-danger p-1 fas fa-trash-alt',type:'button'}));
@@ -112,12 +124,12 @@ function saveList(){
 
 function loadList(){
   $('#sortable').text(''); //clear the sortable list
-  var listArr = JSON.parse(localStorage.getItem('CityList'))
-  if(listArr) {
+  var listArr = JSON.parse(localStorage.getItem('CityList')) //get the data from local storage
+  if(listArr) { // confirm the list is not null
     for (var i = 0; i < listArr.length; i++){
       addSearch(listArr[i]);
     }
-}
+  }
 }
 
 loadList();
@@ -125,7 +137,6 @@ loadList();
 
 // remove item from the sortable list
 $('#sortable').on('click', 'i', function(event){
-    console.log("Trash Clicked")
     $(event.target).parent().remove()
     saveList();
 })
@@ -153,6 +164,9 @@ $('.form-group').on('submit', function(event){
   getWeather(true);
 })
 
+
+$( "#sortable" ).on('click', saveList());
+
 // function for jquery-ui tooltip
 $( function() {
     $('#ask').tooltip();
@@ -161,5 +175,6 @@ $( function() {
 $( function() {
   $( "#sortable" ).sortable();
   $( "#sortable" ).sortable({cursor: 'move'});
+  $( "#sortable" ).sortable({update: function(event, ui) {saveList()}});
   $( "#sortable" ).disableSelection();
 });
